6171d2d44fb0c9f63d06ddba75738fafcd7bc578
nomadlife_15w <joonwoo3816@gmail.com>
2019-12-19 11:27:30 +0900

    Updates

---
+++ precache-manifest.d7cb8f30178afccfa551698ca3ddcb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3fd8aaa426aef6048aa147a49861cba",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "15a41d6e53be5bc3801f",
+    "url": "/react_todo/static/css/main.d84f1f3c.chunk.css"
+  },
+  {
+    "revision": "dcf1c945b919ba6ccf87",
+    "url": "/react_todo/static/js/2.b276fa00.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/react_todo/static/js/2.b276fa00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15a41d6e53be5bc3801f",
+    "url": "/react_todo/static/js/main.22387f87.chunk.js"
+  },
+  {
+    "revision": "348b1be00bcdeb1aa134",
+    "url": "/react_todo/static/js/runtime-main.8ccb84c4.js"
+  }
+]);
